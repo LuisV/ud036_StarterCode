880b89e6839861d430ba9deab859f1fb67937052
LuisV <lvalencia@csumb.edu>
2018-01-13 13:00:55 -0800

    Finally fixed formatting

--- media.py
+++ media.py
@@ -1,8 +1,8 @@
 # Definition for the Movie object
 class Movie():
     # Initializes necessary variables for a Movie object
-    def __init__
-    (self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
